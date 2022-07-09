768744d2a95d60875e2d1a1ab81384c47539f4e6
Eddú Meléndez <eddu.melendez@gmail.com>
2022-07-09 18:56:34 -0500

    Initial commit

---
+++ src/test/java/com/example/codespacesgradle/CodespacesGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codespacesgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodespacesGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
